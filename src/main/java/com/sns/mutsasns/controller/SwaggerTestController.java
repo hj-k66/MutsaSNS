f54d01ff774e411be2cf7d40df96a5c9ad2a9e29
hjx66 <hjx66@g.cnu.ac.kr>
2022-12-20 15:02:54 +0900

    fix: api 엔드포인트 수정 (api/hello >> api/v1/hello)

--- src/main/java/com/sns/mutsasns/controller/SwaggerTestController.java
+++ src/main/java/com/sns/mutsasns/controller/SwaggerTestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SwaggerTestController {
 
-    @GetMapping("/api/hello")
+    @GetMapping("/api/v1/hello")
     public String hello(){
         return "hello";
     }
