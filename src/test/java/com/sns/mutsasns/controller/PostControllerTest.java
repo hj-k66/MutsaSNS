d278f2c024d30f064fb10a06dd7e1f52484c60fd
hjx66 <hjx66@g.cnu.ac.kr>
2022-12-27 02:36:30 +0900

    test: 포스트 리스트 조회 Contoller 테스트

--- src/test/java/com/sns/mutsasns/controller/PostControllerTest.java
+++ src/test/java/com/sns/mutsasns/controller/PostControllerTest.java
@@ -1,15 +1,23 @@
 package com.sns.mutsasns.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sns.mutsasns.configuration.EncrypterConfig;
 import com.sns.mutsasns.domain.dto.posts.PostCreateRequest;
 import com.sns.mutsasns.domain.dto.posts.PostDto;
 
+import com.sns.mutsasns.domain.dto.user.UserDto;
 import com.sns.mutsasns.service.PostService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -17,10 +25,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -36,6 +43,10 @@ class PostControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @MockBean
+    EncrypterConfig EncrypterConfig;
+
 
     @Test
     @DisplayName("포스트 작성 성공")
@@ -97,6 +108,29 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.result.userName").exists())
                 .andExpect(jsonPath("$.result.createdAt").exists())
                 .andExpect(jsonPath("$.result.updatedAt").exists());
+
+    }
+
+    @Test
+    @DisplayName("포스트 목록 조회 성공")
+    @WithMockUser
+    void getPostList_success() throws Exception {
+        when(postService.getAllPosts(any())).thenReturn(mock(Page.class));
+
+        mockMvc.perform(get("/api/v1/posts")
+                        .param("page", "0")
+                        .param("size", "3")
+                        .param("sort", "createdAt,desc"))
+                .andExpect(status().isOk());
+
+        ArgumentCaptor<Pageable> pageableCaptor = ArgumentCaptor.forClass(Pageable.class);
+
+        verify(postService).getAllPosts(pageableCaptor.capture());
+        PageRequest pageable = (PageRequest) pageableCaptor.getValue();
+
+        Assertions.assertEquals(0, pageable.getPageNumber());
+        Assertions.assertEquals(3, pageable.getPageSize());
+        Assertions.assertEquals(Sort.by("createdAt", "desc"), pageable.withSort(Sort.by("createdAt", "desc")).getSort());
 
     }
 }
