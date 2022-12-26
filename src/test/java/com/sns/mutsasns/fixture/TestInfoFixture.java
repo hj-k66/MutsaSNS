f7efb12d473f2257cdef26f8ace162ea7ea4aae5
hjx66 <hjx66@g.cnu.ac.kr>
2022-12-26 23:44:36 +0900

    test: 포스트 작성 Service 테스트 - 등록 성공 - 등록 실패 : 유저가 존재하지 않는 경우

---
+++ src/test/java/com/sns/mutsasns/fixture/TestInfoFixture.java
@@ -0,0 +1,30 @@
+package com.sns.mutsasns.fixture;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class TestInfoFixture {
+    public static TestInfo get() {
+        TestInfo info = new TestInfo();
+        info.setPostId(1);
+        info.setUserId(1);
+        info.setUserName("user");
+        info.setPassword("password");
+        info.setTitle("title");
+        info.setBody("body");
+        return info;
+    }
+
+    @Getter
+    @Setter
+    public static class TestInfo {
+        private Integer postId;
+        private Integer userId;
+        private String userName;
+        private String password;
+        private String title;
+        private String body;
+    }
+
+}
+
