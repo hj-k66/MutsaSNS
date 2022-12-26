e9124b3de1528d4d26ea77bf6878bae2d059e464
hjx66 <hjx66@g.cnu.ac.kr>
2022-12-27 01:04:36 +0900

    refactor: 생성날짜, 수정날짜 포맷 변경

--- src/main/java/com/sns/mutsasns/domain/entity/BaseEntity.java
+++ src/main/java/com/sns/mutsasns/domain/entity/BaseEntity.java
@@ -5,10 +5,9 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @MappedSuperclass
@@ -21,4 +20,16 @@ public class BaseEntity {
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    public void onPrePersist(){
+        this.createdAt = LocalDateTime.parse(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")));
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    public void onPreUpdate(){
+        this.updatedAt = LocalDateTime.parse(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")));
+    }
 }
