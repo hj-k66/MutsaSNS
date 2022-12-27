72157adef61c5f954b4e473ef60f47bd3025640b
hjx66 <hjx66@g.cnu.ac.kr>
2022-12-27 10:03:58 +0900

    feat: 포스트 수정 기능 구현

--- src/main/java/com/sns/mutsasns/service/PostService.java
+++ src/main/java/com/sns/mutsasns/service/PostService.java
@@ -1,6 +1,6 @@
 package com.sns.mutsasns.service;
 
-import com.sns.mutsasns.domain.dto.posts.PostCreateRequest;
+import com.sns.mutsasns.domain.dto.posts.PostWriteRequest;
 import com.sns.mutsasns.domain.dto.posts.PostDto;
 import com.sns.mutsasns.domain.entity.Post;
 import com.sns.mutsasns.domain.entity.User;
@@ -13,6 +13,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 
 @Service
 @RequiredArgsConstructor
@@ -20,7 +22,7 @@ public class PostService {
     private final PostRepository postRepository;
     private final UserRepository userRepository;
 
-    public PostDto create(PostCreateRequest request, String userName){
+    public PostDto create(PostWriteRequest request, String userName){
         User user = userRepository.findByUserName(userName)
                 .orElseThrow(() -> new SNSException(ErrorCode.USERNAME_NOT_FOUND));
         Post post = Post.builder()
@@ -45,5 +47,27 @@ public class PostService {
     public Page<PostDto> getAllPosts(Pageable pageable) {
         Page<Post> posts = postRepository.findAll(pageable);
         return posts.map(Post::toDto);
+    }
+
+    public PostDto modify(Long id, PostWriteRequest postWriteRequest, String userName) {
+        //포스트 존재 x
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new SNSException(ErrorCode.POST_NOT_FOUND));
+        //유저 존재 x
+        User user = userRepository.findByUserName(userName)
+                .orElseThrow(() -> new SNSException(ErrorCode.USERNAME_NOT_FOUND));
+        //포스트 작성자 != 유저
+        if(!Objects.equals(post.getUser().getId(), user.getId())){
+            throw new SNSException(ErrorCode.INVALID_PERMISSION);
+        }
+
+        post.setTitle(postWriteRequest.getTitle());
+        post.setBody(postWriteRequest.getBody());
+        Post savedPost = postRepository.saveAndFlush(post);
+
+        return PostDto.builder()
+                .message("포스트 수정 완료")
+                .postId(savedPost.getId())
+                .build();
     }
 }
