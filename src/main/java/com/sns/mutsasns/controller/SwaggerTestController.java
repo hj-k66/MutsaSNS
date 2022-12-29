75060f2453947bf0032f3e1904802ac44afcdd85
hjx66 <hjx66@g.cnu.ac.kr>
2022-12-29 09:48:08 +0900

    feat: 자릿수 합 구하기 기능 구현 GET /api/v1/hello/{num}

--- src/main/java/com/sns/mutsasns/controller/SwaggerTestController.java
+++ src/main/java/com/sns/mutsasns/controller/SwaggerTestController.java
@@ -1,17 +1,27 @@
 package com.sns.mutsasns.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/hello")
 public class SwaggerTestController {
 
-    @GetMapping("/api/v1/hello")
+    @GetMapping("/{num}")
+    public ResponseEntity<Integer> sumOfDigit(@PathVariable int num){
+        int sum = 0;
+        while(num > 0){
+            sum += num%10;
+            num /= 10;
+        }
+        return ResponseEntity.ok().body(sum);
+    }
+
+    @GetMapping("")
     public String hello(){
         return "김희정";
-    }
-    @GetMapping("/api/v1/bye")
-    public String bye(){
-        return "bye";
     }
 }
