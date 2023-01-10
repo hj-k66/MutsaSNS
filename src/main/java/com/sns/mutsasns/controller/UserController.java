3b22779c8af2f887a9246881e508a11bc2490b5c
hjx66 <hjx66@g.cnu.ac.kr>
2023-01-10 16:34:31 +0900

    feat: ApiOperation을 사용으로 Swagger에 Controller 설명 추가

--- src/main/java/com/sns/mutsasns/controller/UserController.java
+++ src/main/java/com/sns/mutsasns/controller/UserController.java
@@ -3,6 +3,7 @@ package com.sns.mutsasns.controller;
 import com.sns.mutsasns.domain.dto.*;
 import com.sns.mutsasns.domain.dto.user.*;
 import com.sns.mutsasns.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,13 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
+    @Operation(summary = "회원가입")
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest userJoinRequest){
         UserDto userDto = userService.join(userJoinRequest);
         return Response.success(new UserJoinResponse(userDto.getId(),userDto.getUserName()));
     }
-
+    @Operation(summary = "로그인")
     @PostMapping("/login")
     public Response<UserLoginResponse> login(@RequestBody UserLoginRequest userLoginRequest){
         String token = userService.login(userLoginRequest);
